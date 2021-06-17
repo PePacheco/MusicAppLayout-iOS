4ee61e69067373a87c932a17da7fc7f5ee439dc8
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-06-17 18:22:59 -0300

    base

---
+++ MusicAppLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicAppLayout
+//
+//  Created by Thaís Fernandes on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
