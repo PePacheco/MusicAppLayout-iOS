5d33829a5ae700b6c94e2708286fe66564d252d8
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-06-22 13:40:59 -0300

    added link to Player

--- MusicAppLayout/PlayerViewController.swift
+++ MusicAppLayout/PlayerViewController.swift
@@ -11,24 +11,12 @@ class PlayerViewController: UIViewController {
             
     @IBOutlet weak var slider: UISlider!
     
-    let sliderThumb = UIImage(systemName: "circle.fill")
+    let sliderThumb = UIImage(named: "thumb_icon")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        slider.setThumbImage(UIImage(named: "thumb_icon"), for: .normal)
-        
-        // Do any additional setup after loading the view.
+        slider.setThumbImage(sliderThumb, for: .normal)
+        slider.setThumbImage(sliderThumb, for: .highlighted)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
