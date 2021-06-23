5a90039c7d12a97d45660381eac03c97f222331c
Thaís Fernandes <throdriguesfernandes@gmail.com>
2021-06-23 16:09:05 -0300

    base

---
+++ MusicAppLayout/Controllers/QueueViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QueueViewController.swift
+//  MusicAppLayout
+//
+//  Created by Thaís Fernandes on 22/06/21.
+//
+
+import UIKit
+
+class QueueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MusicAppLayout/Views/QueueCell.swift
@@ -0,0 +1,8 @@
+//
+//  QueueCell.swift
+//  MusicAppLayout
+//
+//  Created by Thaís Fernandes on 23/06/21.
+//
+
+import Foundation
